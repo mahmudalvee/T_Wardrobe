683f3d61fb5105319c3d7dd788c33e2483f4a65a
mahmudalvee <mahmud2.alvee@gmail.com>
2022-11-17 18:56:47 +0600

    Add files via upload

---
+++ T_Wardrobe/includes/footer.php
@@ -0,0 +1,122 @@
+<div id="footer"><!-- footer begin-->
+	
+	<div class="container"><!-- container begin-->
+		
+			<div class="row"><!-- row begin-->
+				
+					<div class="col-sm-6 col-md-3">
+
+							<h4>Pages</h4>
+						
+							<ul>
+								<li><a href="cart.php">Shopping Cart</a></li>
+								<li><a href="shop.php">Shop</a></li>
+								<li><a href="contact.php">Contact Us</a></li>
+								<li><a href="checkout.php">My Account</a></li>
+							</ul>
+
+							<hr>
+
+							<h4>User Section</h4>
+
+							<ul>
+								<li><a href="checkout.php">Login</a></li>
+								<li><a href="customer_register.php">Register</a></li>
+							</ul>
+
+							<hr class="hidden-md hidden-lg hidden-sm">
+
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+						
+							<h4>Top Product Categories</h4>
+
+							<ul>
+								<li><a href="#">Jackets</a></li>
+								<li><a href="#">Jerseys</a></li>
+								<li><a href="#">Coats</a></li>
+								<li><a href="#">Polo Shirts</a></li>
+								<li><a href="#">Customizable T-Shirts</a></li>
+							</ul>
+
+							<hr class="hidden-md hidden-lg">
+
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+						
+							<h4>Find Us:</h4>
+
+							<p>
+								
+									<strong>t_wardrobe@gmail.com</strong>
+									<br/>Mirpur
+									<br/>Dhanmondi
+									<br/>01750414709
+									<br/>inzamum.inu@gmail.com/<br>mahmud2.alvee@gmail.com
+
+							</p>
+
+							<a href="contact.php"><strong>Check Our Contact Page</strong></a>
+
+							<hr class="hidden-md hidden-lg">
+
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+
+							<h4>Get The News</h4>
+						
+							<p class="text-muted">
+								Dont miss our latest update products
+							</p>
+
+							<form action="" method="post"><!-- form begin-->
+								<div class="input-group">
+									
+										<input type="text" class="form-control" name="email">
+											
+										<button type="submit" name="btn btn-default"><strong>Subscribe</strong></button>
+
+								</div>
+							</form><!-- form finish -->
+
+							<hr>
+
+							<h4>Keep In Touch</h4>
+
+							<p class="social">
+								<a href="#" class="fa fa-facebook"></a>
+								<a href="#" class="fa fa-twitter"></a>
+								<a href="#" class="fa fa-google-plus"></a>
+								<a href="#" class="fa fa-envelope"></a>
+							</p>
+
+					</div>
+
+			</div><!-- row finish-->
+
+	</div><!-- footer finish-->
+
+</div><!-- footer finish-->
+
+<div class="copyright"><!-- copyright begin-->
+	
+		<div class="container"><!-- container begin-->
+			
+				<div class="col-md-6">
+					
+						<p class="pull-left">&copy; 2020 T-Wardrobe All Rights Reserve</p>
+
+				</div>
+
+				<div class="col-md-6">
+					
+						<p class="pull-right">Theme By: Inzamum & Fahmeed</p>
+
+				</div>
+
+		</div><!-- container finish-->
+
+</div><!-- copyright finish-->
---
+++ T_Wardrobe/index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+
+    <title> T Wardrobe - An Online T-Shirt Shopping Store </title>
+    <link rel="shortcut icon" type="image/png" href="images/icon2.png">
+</head>
+<body>
+
+	<div class="whole_body"><!-- whole_body begin -->
+		
+			<nav><!-- top navigation menu begin -->
+
+					<input type="checkbox" id="check">
+					<label for="check">
+						<i class="fa fa-bars" id="btn"></i>
+						<i class="fa fa-times" id="cancel"></i>
+					</label>
+
+					<img src="images/text_logo.png" alt="Brand Logo">
+		
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="shop.php">Shop</a>
+						</li>
+						<li>
+							<a href="abouut_us.php">About Us</a>
+						</li>
+						<li>
+							<a href="contact.php">Contact Us</a>
+						</li>
+						<li>
+							<a href="customer_register.php">Register</a>
+						</li>
+						<li>
+							<a href="checkout.php">Login</a>
+						</li>
+						<li>
+							<a href="checkout.php">My Account</a>
+						</li>
+						<li>
+							<a href="cart.php">Cart</a>
+						</li>
+					</ul>
+
+			</nav><!-- top navigation menu finish -->
+
+		
+			<div class="container" id="banner_area"><!-- container begin -->
+				
+				<div id="myCarousel" class="carousel slide" data-ride="carousel"><!-- carousel slide begin-->
+					
+						<ol class="carousel-indicators">
+								<!--Indicators -->
+								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#myCarousel" data-slide-to="1"></li>
+								<li data-target="#myCarousel" data-slide-to="2"></li>
+
+						</ol>
+						<!-- wrapper for slides -->
+						<div id="wrapper" class="carousel-inner"><!-- carousel-inner begin -->
+							
+								<div class="item active">
+									
+									<img src="images/banner1_alt.jpg" alt="Banner Image">
+
+								</div>
+
+								<div class="item">
+									
+									<img src="images/banner2.jpg" alt="Banner Image">
+
+								</div>
+
+								<div class="item">
+									
+									<img src="images/banner3.jpg" alt="Banner Image">
+
+								</div>
+
+						</div><!-- carousel-inner finish -->
+
+								<!-- Left and Right controls -->
+						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+									
+								<span class="glyphicon glyphicon-chevron-left"></span>
+								<span class="sr-only">Previous</span>
+
+						</a>
+
+						<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							
+								<span class="glyphicon glyphicon-chevron-right"></span>
+								<span class="sr-only">Next</span>
+
+						</a>
+
+				</div><!-- carousel slide finish-->
+
+			</div><!-- container finish -->
+
+
+			<div id="advantages"><!--advantages begin-->
+				
+					<div class="container"><!--container begin-->
+						
+							<div class="same-height-row"><!--same-height-row begin-->
+								
+									<div class="col-sm-4"><!--col-sm-4 begin-->
+
+										<div class="box same-height"><!--box-same-height begin-->
+											
+												<div class="icon">
+													
+														<i class="fa fa-heart"></i>
+
+												</div>
+
+												<h3><a href="#">Best Offer</a></h3>
+												<p>We know to provide the best possible service ever.</p>
+
+										</div><!--box-same-height finish-->
+
+									</div><!--col-sm-4 finish-->
+
+									<div class="col-sm-4"><!--col-sm-4 begin-->
+
+										<div class="box same-height"><!--box-same-height begin-->
+											
+												<div class="icon">
+													
+														<i class="fa fa-tag"></i>
+
+												</div>
+
+												<h3><a href="#">Best Prices</a></h3>
+												<p>Compare us with another site,who have the best prices.</p>
+
+										</div><!--box-same-height finish-->
+
+									</div><!--col-sm-4 finish-->
+
+									<div class="col-sm-4"><!--col-sm-4 begin-->
+
+										<div class="box same-height"><!--box-same-height begin-->
+											
+												<div class="icon">
+													
+														<i class="fa fa-thumbs-up"></i>
+
+												</div>
+
+												<h3><a href="#">100% Original</a></h3>
+												<p>We offer you the best and original products and services.</p>
+
+										</div><!--box-same-height finish-->
+
+									</div><!--col-sm-4 finish-->
+
+							</div><!--same-height-row finish-->
+
+					</div><!--container finish-->
+
+			</div><!--advantages finish-->
+
+
+			<div id="product_storefront"><!--product_storefront begin-->
+				
+					<div class="box"><!--box begin-->
+						
+							<div class="container"><!--container begin-->
+								
+									<div class="col-md-12">
+										
+											<h2>
+												Our Latest Products
+											</h2>
+
+									</div>
+
+							</div><!--container finish-->
+
+					</div><!--box finish-->
+
+			</div><!--product_storefront finish-->
+
+
+			<div id="content" class="container"><!--container begin-->
+				
+					<div class="row"><!--row begin-->
+						
+							<div class="col-sm-4 col-sm-6 single">
+								
+									<div class="product"><!--product begin-->
+										
+											<a href="details.php">
+												
+													<img class="img-responsive" src="product_images/product_image1.png" alt="Product Image">
+
+											</a>
+
+											<div class="text"><!--text begin-->
+												
+													<h3>
+														<a href="details.php">
+															Logo T-Shirt
+														</a>
+													</h3>
+
+													<p class="price">Tk.300</p>
+
+													<p class="button">
+														
+															<a href="details.php" class="btn btn-default">View Details</a>
+
+															<a href="details.php" class="btn btn-primary">
+																
+																<i class="fa fa-shopping-cart">
+																	Add To Cart
+																</i>
+
+															</a>
+
+													</p>
+
+											</div><!--text finish-->
+
+									</div><!--product finish-->
+
+							</div>
+
+							<div class="col-sm-4 col-sm-6 single">
+								
+									<div class="product"><!--product begin-->
+										
+											<a href="details.php">
+												
+													<img class="img-responsive" src="product_images/product_image2.png" alt="Product Image">
+
+											</a>
+
+											<div class="text"><!--text begin-->
+												
+													<h3>
+														<a href="details.php">
+															Logo T-Shirt
+														</a>
+													</h3>
+
+													<p class="price">Tk.300</p>
+
+													<p class="button">
+														
+															<a href="details.php" class="btn btn-default">View Details</a>
+
+															<a href="details.php" class="btn btn-primary">
+																
+																<i class="fa fa-shopping-cart">
+																	Add To Cart
+																</i>
+
+															</a>
+
+													</p>
+
+											</div><!--text finish-->
+
+									</div><!--product finish-->
+
+							</div>
+
+							<div class="col-sm-4 col-sm-6 single">
+								
+									<div class="product"><!--product begin-->
+										
+											<a href="details.php">
+												
+													<img class="img-responsive" src="product_images/product_image1.png" alt="Product Image">
+
+											</a>
+
+											<div class="text"><!--text begin-->
+												
+													<h3>
+														<a href="details.php">
+															Logo T-Shirt
+														</a>
+													</h3>
+
+													<p class="price">Tk.300</p>
+
+													<p class="button">
+														
+															<a href="details.php" class="btn btn-default">View Details</a>
+
+															<a href="details.php" class="btn btn-primary">
+																
+																<i class="fa fa-shopping-cart">
+																	Add To Cart
+																</i>
+
+															</a>
+
+													</p>
+
+											</div><!--text finish-->
+
+									</div><!--product finish-->
+
+							</div>
+
+							<div class="col-sm-4 col-sm-6 single">
+								
+									<div class="product"><!--product begin-->
+										
+											<a href="details.php">
+												
+													<img class="img-responsive" src="product_images/product_image1.png" alt="Product Image">
+
+											</a>
+
+											<div class="text"><!--text begin-->
+												
+													<h3>
+														<a href="details.php">
+															Logo T-Shirt
+														</a>
+													</h3>
+
+													<p class="price">Tk.300</p>
+
+													<p class="button">
+														
+															<a href="details.php" class="btn btn-default">View Details</a>
+
+															<a href="details.php" class="btn btn-primary">
+																
+																<i class="fa fa-shopping-cart">
+																	Add To Cart
+																</i>
+
+															</a>
+
+													</p>
+
+											</div><!--text finish-->
+
+									</div><!--product finish-->
+
+							</div>
+
+					</div><!--row begin-->
+
+			</div><!--container finish-->
+
+			<?php
+
+					include("includes/footer.php");
+
+			?>
+
+	</div><!-- whole_body finish-->
+
+
+	
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	
+</body>
+</html>
